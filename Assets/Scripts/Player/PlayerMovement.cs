5ac8a4d2ddc2509037786f499fea6e24ebf73507
Nenad Stojanovikj <nekk1@live.com>
2017-04-01 01:32:25 +0200

    Gehen nach Hause

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,26 @@ public class PlayerMovement : PlayerBehaviour {
 
     public float Speed;
     private Rigidbody2D rb;
+    private Animator animator;
+    public Vector2 Velocity;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        Velocity = new Vector2();
     }
 
     void FixedUpdate () {
         var horizontal = PlayerInput.GetAxis("Horizontal", Player) * Speed;
         var vertical = PlayerInput.GetAxis("Vertical", Player) * Speed;
-        rb.velocity = new Vector3(horizontal, vertical, 0);
+        Velocity = new Vector2(horizontal, vertical);
+        rb.velocity = Velocity;
+        if (Velocity.magnitude == 0) { 
+            animator.Play("workerIdle");
+        } else {
+            animator.Play("workerWalk");
+        }
+        
     }
 }

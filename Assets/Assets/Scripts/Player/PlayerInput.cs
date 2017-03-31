312509ea668f76964a7d2cfa8c7cecf7f5c8f05c
Nenad Stojanovikj <nekk1@live.com>
2017-03-31 21:00:12 +0200

    Add player input

---
+++ Assets/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public void Start()
+    {
+        var joys = Input.GetJoystickNames();
+        Debug.Log("HELLO");
+        for (var i = 0; i < joys.Length; i++)
+        {
+            Debug.Log(joys[i]);
+        }
+    }
+
+    public static float GetAxis(string axis, Player player)
+    {
+        var p = player.ToString().Substring(1, 1);
+        return Input.GetAxis(axis + "_" + p);
+    }
+
+    public static bool GetButton(string keyName, Player player)
+    {
+        var p = player.ToString().Substring(1, 1);
+        return Input.GetButton(keyName + "_" + p);
+    }
+
+}
--- Assets/Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Player
+{
+    P1,
+    P2,
+    P3,
+    P4
+};
+
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Player player;
+
+    // Update is called once per frame
+    void Update () {
+        var horizontal = PlayerInput.GetAxis("Horizontal", player);
+        var vertical = PlayerInput.GetAxis("Vertical", player);
+
+        transform.Translate(horizontal, vertical, 0);
+    }
 }

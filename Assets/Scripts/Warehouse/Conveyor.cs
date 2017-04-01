d6cb27e87515bfffa3f7c743c44e532a018a469b
Tomaz Zinko <tomaz.zinko@gmail.com>
2017-04-01 21:19:47 +0200

    turn theeeem

--- Assets/Scripts/Warehouse/Conveyor.cs
+++ Assets/Scripts/Warehouse/Conveyor.cs
@@ -5,44 +5,56 @@ using UnityEngine;
 public class Conveyor : MonoBehaviour {
     public Transform SpawnPoint;
 
-    private HashSet<Ingredient> ingredients = new HashSet<Ingredient>();
-    private HashSet<Ingredient> takenOutIngredients = new HashSet<Ingredient>();
+    private HashSet<Pickable> items = new HashSet<Pickable>();
+    private HashSet<Pickable> takenOutIngredients = new HashSet<Pickable>();
 
     public float MinSpawnTime = 1;
     public float MaxSpawnTime = 2;
 
-    public GameObject[] Ingredients;
+    public float Speed = 1f;
+
+    public GameObject[] Items;
+    public int[] ItemsWeight;
+
+    private int totalWeight = 0;
 
     public float lastSpawn;
 
     // Use this for initialization
     void Start () {
-		
+		foreach(int weight in ItemsWeight)
+        {
+            totalWeight += weight;
+        }
+        if(Items.Length != ItemsWeight.Length)
+        {
+            Debug.LogError("Conveyor Items and Itemsweight are not same length");
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var i = collision.GetComponent<Ingredient>();
+        var i = collision.GetComponent<Pickable>();
         if (i != null)
         {
-            this.ingredients.Add(i);
+            this.items.Add(i);
         }
 
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        var i = collision.GetComponent<Ingredient>();
-        this.ingredients.Remove(i);
+        var i = collision.GetComponent<Pickable>();
+        this.items.Remove(i);
 
     }
     // Update is called once per frame
     void Update () {
         spawnIngredients(Time.deltaTime);
 
-        foreach(var i in ingredients)
+        foreach(var i in items)
         {
-            i.transform.Translate(new Vector3(0, -1 * Time.deltaTime, 0));
+            i.transform.Translate(new Vector3(0, -1 * Time.deltaTime * Speed, 0), Space.World);
         }
 	}
 
@@ -51,11 +63,22 @@ public class Conveyor : MonoBehaviour {
         lastSpawn -= delta;
         if (this.lastSpawn < 0)
         {
-            var i = Random.Range(0, Ingredients.Length);
+            var rngnum = Random.Range(0, totalWeight);
+
+            int i = 0;
+            int curWeight = ItemsWeight[0];
+            while (rngnum > curWeight)
+            {
+                i++;
+                curWeight += ItemsWeight[i];
+            }
+
+
             var pos = SpawnPoint.position;
             pos.x += Random.Range(-.1f, .1f);
 
-            GameObject.Instantiate(Ingredients[i], pos, new Quaternion());
+
+            GameObject.Instantiate(Items[i], pos, Quaternion.Euler(0,0,Random.Range(0, 360)));
             this.lastSpawn = Random.Range(MinSpawnTime, MaxSpawnTime);
         }
     }

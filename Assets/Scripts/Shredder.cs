a919798981605fbebd7921652c30739ce06bcbce
EmilyClaudet <emily@kano.me>
2017-04-01 21:51:49 +0200

    we can shred things now

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+	// Destroys what ever gameobject collides with it
+	void OnTriggerEnter2D (Collider2D col) {
+		Debug.Log("There has been a collision");
+		Destroy(col.gameObject);
+	}
+}

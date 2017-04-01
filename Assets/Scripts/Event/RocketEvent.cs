5f844b555b6fdac6fd2a9f84aa3e55d4fb89bc5f
Kieran <me@kieranajp.co.uk>
2017-04-02 00:50:12 +0200

    Add RocketEvent

---
+++ Assets/Scripts/Event/RocketEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketEvent : RandomEvent {
+
+	public float Extension = 5.0f;
+
+	public string Name     = "Rocket";
+	public string Avatar   = "rocket.jpg";
+	public string Message  = "Another round of seed funding has been completed! Time extended!";
+
+	public override void Fire() {
+		Debug.Log (string.Format("Firing {0}!", Name));
+
+		var timer = GameObject.FindObjectOfType<Timer> ();
+
+		timer.AddTime (Extension);
+	}
+}

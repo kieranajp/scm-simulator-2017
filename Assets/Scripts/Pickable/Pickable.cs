510a356e72658ee05a0b126f9bde141a6de75855
Tomaz Zinko <tomaz.zinko@gmail.com>
2017-04-01 22:06:42 +0200

    5 second rule

--- Assets/Scripts/Pickable/Pickable.cs
+++ Assets/Scripts/Pickable/Pickable.cs
@@ -5,6 +5,11 @@ public class Pickable : MonoBehaviour
     public bool IsPickedUp;
     public bool CanBePicked = true;
 
+    public bool FiveSecondRule = true;
+    public float MaxTimeOnFloor = 5f;
+    Vector3 lastPosition = Vector3.zero;
+    float timeOnFloor = 0;
+
     virtual public void PickUp()
     {
         IsPickedUp = true;
@@ -13,5 +18,24 @@ public class Pickable : MonoBehaviour
     virtual public void PutDown()
     {
         IsPickedUp = false;
+    }
+
+    void FixedUpdate()
+    {
+        float speed = (transform.position - lastPosition).magnitude;
+        lastPosition = transform.position;
+
+        if (speed < 0.01f && !IsPickedUp && FiveSecondRule)
+        {
+            timeOnFloor += Time.fixedDeltaTime;
+        }
+        else {
+            timeOnFloor = 0;
+        }
+
+        if(timeOnFloor > MaxTimeOnFloor)
+        {
+            Destroy(gameObject);
+        }
     }
 }

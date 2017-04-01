23701fb58803845f9636598ce7436eee93d89153
Nenad Stojanovikj <nekk1@live.com>
2017-04-01 23:09:53 +0200

    TRUCKING FINALLY

--- Assets/Scripts/Truck/TruckMovement.cs
+++ Assets/Scripts/Truck/TruckMovement.cs
@@ -1,30 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class TruckMovement : MonoBehaviour {
 
+    public Transform wheel;
+    public Transform lorry;
     public Transform door;
 
     private float _curSeconds = 0;
+    private bool _doorOpening = true;
+    private bool _isFinishedRolling = false;
     Vector3 targetPosition = new Vector3(0, 0, 0);
 
 	void Start () {
-        transform.localPosition = new Vector3(-3, 0, 0);
         StartCoroutine(MoveToBack());
+        StartCoroutine(OpenDoor());
 	}
 
     private void Update()
     {
-        transform.localPosition += new Vector3(0, Mathf.Sin(Time.unscaledTime * 20) / 200);
+        if (_isFinishedRolling)
+        {
+            lorry.transform.localPosition = new Vector3(0, Mathf.Sin(Time.time * 20) / 100);
+            door.transform.localPosition = new Vector3(0, Mathf.Sin(Time.time * 20) / 100);
+        }
     }
 
     private IEnumerator MoveToBack()
     {
-        while (_curSeconds < 4)
+        while (_curSeconds < 3)
         {
             yield return null;
-            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.unscaledDeltaTime);
+            lorry.transform.localPosition = Vector3.Lerp(lorry.transform.localPosition, targetPosition, Time.unscaledDeltaTime * 2);
+            wheel.transform.localPosition = Vector3.Lerp(wheel.transform.localPosition, targetPosition, Time.unscaledDeltaTime * 2);
             _curSeconds += Time.unscaledDeltaTime;
         }
+
+        _isFinishedRolling = true;
+        Debug.Log("HERE");
     }
+
+    private IEnumerator OpenDoor()
+    {
+        yield return new WaitForSeconds(1);
+        while (_doorOpening)
+        {
+            door.transform.localScale = new Vector3(door.transform.localScale.x + Time.unscaledDeltaTime, 1, 1);
+            if (door.transform.localScale.x > 0.95f)
+            {
+                _doorOpening = false;
+            }
+
+            yield return null;
+        }
+    }
+
 }

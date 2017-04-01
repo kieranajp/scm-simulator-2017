a4d1bc18119281196e86c953fd31651eb30e9718
Nenad Stojanovikj <nekk1@live.com>
2017-04-01 21:47:23 +0200

    Fix

--- Assets/Scripts/StartGame/StartGame.cs
+++ Assets/Scripts/StartGame/StartGame.cs
@@ -1,16 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-// Time.timeScale = 0;
+    public string[] Announcements;
+    public Text announcement;
+    private float _currentTime = 0;
+    private int currentAnnouncement = 0;
+    private Quaternion originalRotation;
+    private int _currentSkipFrames = 0;
+    private int _skipFrames = 4;
+
+    // Use this for initialization
+    void Start () {
+        Time.timeScale = 0;
+        originalRotation = announcement.transform.rotation;
     }
 
     private void Update()
     {
+        if (currentAnnouncement == Announcements.Length) {
+            announcement.text = "";
+            Time.timeScale = 1;
+            this.enabled = false;
+            return;
+        }
+
+        if (_currentSkipFrames++ > _skipFrames)
+        {
+            _currentSkipFrames = 0;
+        }
+
+        _currentTime += Time.unscaledDeltaTime;
+
+        announcement.transform.rotation = Quaternion.Euler(0, 0, originalRotation.eulerAngles.z + UnityEngine.Random.Range(0, 13));
         
+        if (_currentTime > 1)
+        {
+            _currentTime = 0;
+            Next();
+        }
+    }
+
+    private void Next()
+    {
+        announcement.text = Announcements[currentAnnouncement++];
     }
 }
